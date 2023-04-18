bd1164029da3664b4441906237f40a7ecf388644
ShaungBhone <phonelaayy@gmail.com>
2023-04-18 12:35:17 +0630

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <section>
+    <section class="relative">
       <img
-        class="absolute w-2/5 right-0"
+        class="absolute w-1/3 right-0 top-0"
         src="~/assets/images/my-photo.jpg"
         alt="shaungbhone"
       />
       <div class="container">
-        <p class="text-6xl font-extralight leading-tight lg:w-1/2 pt-48">
-          <span class="p-1 italic">Digital</span>
+        <p class="text-6xl font-extralight leading-snug lg:w-1/2 xl:pt-32 pt-0">
+          <span class="p-1 italic">Digital </span>
           <span class="italic">change-maker</span> <br />
           seeking <span class="bg-cyan-300">next opportunity</span> <br />
           to make an <span class="font-bold">impact</span>. <br />
@@ -16,11 +16,191 @@
         </p>
       </div>
     </section>
-    <section class="container mt-64 relative">
-      <h1 class="text-5xl font-thin">
+    <section class="mt-60 relative">
+      <h1 class="text-6xl font-thin container">
         This is <span class="font-bold">me</span>
       </h1>
-      <div class="w-1/2 bg-cyan-300 h-32 absolute left-0 top-6 -z-10"></div>
+      <div class="w-3/5 bg-cyan-300 h-40 absolute left-0 top-6 -z-10"></div>
+    </section>
+    <section class="container px-96 mt-44">
+      <p class="text-lg">
+        I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+        Just click “Edit Text” or double click me to add your own content and
+        make changes to the font. Feel free to drag and drop me anywhere you
+        like on your page. I’m a great place for you to tell a story and let
+        your users know a little more about you.
+      </p>
+      <p class="mt-14 text-4xl font-thin leading-relaxed">
+        <span>Content Strategy </span>
+        <span class="p-1 bg-gray-900 text-white">Writing</span> <br />
+        <span class="p-1 bg-gray-300">Project Management</span><br />
+        <span>Branding</span> <span>Social Media</span> <br />
+        <span>Events Content</span> <span>Fundraising</span> <br />
+        <span>Creative Initiatives</span>
+      </p>
+      <div class="mt-20">
+        <NuxtLink class="primary-btn" to="/resume">Full Resume</NuxtLink>
+      </div>
+    </section>
+    <section class="mt-10">
+      <div class="flex">
+        <div class="w-3/5"></div>
+        <div class="w-2/5">
+          <img
+            class="grayscale"
+            src="~/assets/images/sb-looks-laptop.jpg"
+            alt="shaungbhone"
+          />
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container -mt-36">
+        <div class="bg-gray-300 ml-48 mr-80 py-16 px-24 space-y-16 relative">
+          <h1 class="text-6xl font-thin leading-tight">
+            I <span class="font-bold">advise</span> companies <br />
+            and my clients in initiatives.
+          </h1>
+          <p class="font-normal text-lg leading-loose">
+            I am dedicated to helping companies and clients navigate the
+            ever-evolving world of technology and software development. With my
+            deep expertise in coding, programming languages, and software
+            architecture, I provide valuable advice and guidance to ensure
+            successful software initiatives.
+          </p>
+          <p class="font-normal text-lg leading-loose">
+            One of my key roles as a developer advisor is to assist companies in
+            selecting the right technologies and platforms for their software
+            projects. This involves understanding their business requirements,
+            evaluating different technologies based on factors such as
+            scalability, performance, and security, and recommending the most
+            suitable options.
+          </p>
+          <div>
+            <NuxtLink class="primary-btn" to="/resume">Get in touch</NuxtLink>
+          </div>
+          <div class="bg-cyan-300 w-24 h-[96%] absolute top-0 -right-12"></div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-24 relative">
+      <h1 class="text-6xl font-thin container">
+        <span>Clients &</span> <br />
+        <span class="font-bold">Collaborations</span>
+      </h1>
+      <div class="w-3/5 bg-cyan-300 h-40 absolute left-0 top-24 -z-10"></div>
+    </section>
+    <section class="container mt-10">
+      <div class="bg-white py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">
+            Trusted by the world’s most innovative teams
+          </h2>
+          <div
+            class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+          >
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+              alt="Transistor"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+              alt="Reform"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+              alt="Tuple"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+              alt="SavvyCal"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+              alt="Statamic"
+              width="158"
+              height="48"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-16">
+      <div class="bg-white py-16">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto lg:mx-0 text-center">
+            <h2 class="font-thin tracking-tight text-gray-900 text-6xl">
+              Recent <span class="font-bold">Blog</span> Posts
+            </h2>
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              Learn how to grow your business with our expert advice.
+            </p>
+          </div>
+          <div
+            class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+          >
+            <article class="flex max-w-xl flex-col items-start justify-between">
+              <div class="flex items-center gap-x-4 text-xs">
+                <time datetime="2020-03-16" class="text-gray-500"
+                  >Mar 16, 2020</time
+                >
+                <a
+                  href="#"
+                  class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >Marketing</a
+                >
+              </div>
+              <div class="group relative">
+                <h3
+                  class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+                >
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Boost your conversion rate
+                  </a>
+                </h3>
+                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                  totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                  Sed exercitationem placeat consectetur nulla deserunt vel.
+                  Iusto corrupti dicta.
+                </p>
+              </div>
+              <div class="relative mt-8 flex items-center gap-x-4">
+                <img
+                  src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                  class="h-10 w-10 rounded-full bg-gray-50"
+                />
+                <div class="text-sm leading-6">
+                  <p class="font-semibold text-gray-900">
+                    <a href="#">
+                      <span class="absolute inset-0"></span>
+                      Michael Foster
+                    </a>
+                  </p>
+                  <p class="text-gray-600">Co-Founder / CTO</p>
+                </div>
+              </div>
+            </article>
+
+            <!-- More posts... -->
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
