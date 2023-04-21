93ce0d4bfcc9843ccd31c0350c7636abe33fc072
ShaungBhone <phonelaayy@gmail.com>
2023-04-21 11:33:48 +0630

    wip

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,5 +1,8 @@
 <template>
+  <main>
     <div>
-        this is post page one
+      <h1 class="text-5xl mt-4 font-semibold tracking-wide">single</h1>
+      <p class="text-sm font-light my-4">single</p>
     </div>
+  </main>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -56,10 +56,10 @@ onMounted(async () => {
                   <h3
                     class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
                   >
-                    <a href="#">
+                    <NuxtLink :to="`/blog/${item.id}`">
                       <span class="absolute inset-0"></span>
                       {{ item.title }}
-                    </a>
+                    </NuxtLink>
                   </h3>
                   <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
                     {{ item.content }}
